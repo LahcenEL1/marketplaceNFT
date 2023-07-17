3f6cae216f7b315f8784bb91824cadde7891658e
LahcenEL1 <lahcen.elouardi@outlook.fr>
2023-07-17 16:35:19 +0200

    upd

--- components/createNFT/CreateNftMain.tsx
+++ components/createNFT/CreateNftMain.tsx
@@ -1,23 +1,25 @@
+import { Box, Typography, Container, Grid } from "@material-ui/core";
 import nftExploreData from "../../data/nftExploreData";
 import NftCard from "../cards/NftCard";
 import UploadFile from "./UploadFile";
 
 const CreateNftMain = () => {
   return (
-    <div className="w-full flex flex-wrap sm:flex-nowrap gap-8 sm:gap-3 xl:gap-6">
-      <div className="w-full lg:w-7/12 xl:w-9/12">
-        {/* Upload File */}
-        <UploadFile />
-      </div>
-      <div className="w-full lg:w-5/12 xl:w-3/12">
-        <h6 className="text-2xl leading-[150%] font-bold dark:text-white text-[var(--color-gray-6)] mb-5 sm:mb-6">
-          Preivew Item
-        </h6>
-
-        {/* Nft Card */}
-        <NftCard data={nftExploreData[0]} />
-      </div>
-    </div>
+    <Container maxWidth="lg">
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={7} lg={9}>
+          <Box border={1} borderColor="grey.300" borderRadius={2} p={2}>
+            <UploadFile />
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={5} lg={3}>
+          <Typography variant="h6" component="h2" gutterBottom>
+            Preview Item
+          </Typography>
+          <NftCard data={nftExploreData[0]} />
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
